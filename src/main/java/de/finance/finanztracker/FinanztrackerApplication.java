2c0107ee9e5ad4b5c90b4ded944e2d8cee209d13
SimbreyAlina <a.simbrey@gmx.de>
2026-04-14 13:40:08 +0200

    .

---
+++ src/main/java/de/finance/finanztracker/FinanztrackerApplication.java
@@ -0,0 +1,13 @@
+package de.finance.finanztracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanztrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanztrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/finance/finanztracker/FinanztrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.finance.finanztracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanztrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
